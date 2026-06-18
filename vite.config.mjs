5817de86c2017328b39c192ca9a7437933d593a8
Jensy <jensy3337@gmail.com>
2026-06-18 12:42:49 +0530

    Fix GitHub Pages app loading

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,15 @@ export default defineConfig({
   base: '/kj/',
   plugins: [react()],
   cacheDir: '.vite-cache',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
