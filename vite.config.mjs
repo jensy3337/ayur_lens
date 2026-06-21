fc5969611b9f01ca46efa5d4349b7b1becc51108
Jensy <jensy3337@gmail.com>
2026-06-21 15:15:38 +0530

    new commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ayur_lens/',
+  base: './',
   plugins: [react()],
   cacheDir: '.vite-cache',
   build: {
